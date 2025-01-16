9880897af64665c7186449ee8dfbf311f0b0b37b
CVerma516 <cv08511@gmail.com>
2025-01-16 14:51:47 +0530

    second-commit

---
+++ ApacheCamelExB/ApacheCamelExB/src/main/java/com/example/ApacheCamelExB/Helloworld.java
@@ -0,0 +1,10 @@
+package com.example.ApacheCamelExB;
+
+import org.springframework.http.converter.json.GsonBuilderUtils;
+
+public class Helloworld {
+    public String hello()
+    {
+        return "hello world";
+    }
+}
